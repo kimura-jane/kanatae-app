8ace0122dcccbd67b2b07491a34f8a73f780071d
木村 <lakokubunji@gmail.com>
2025-12-11 15:13:04 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'kanatake-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/spots.js',
+  '/icon.png',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
